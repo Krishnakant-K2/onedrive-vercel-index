6c39ae7d9f4c2b2a2982483de5a183b545fbb5b9
Krishnakant-K2 <104070803+Krishnakant-K2@users.noreply.github.com>
2022-05-18 13:46:32 +0530

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -50,7 +50,7 @@ module.exports = {
   links: [
     {
       name: 'GitHub',
-      link: 'https://github.com/spencerwooo/onedrive-vercel-index',
+      link: 'https://github.com/Krishnakant-K2/onedrive-vercel-index',
     },
     {
       name: 'Telegram',
